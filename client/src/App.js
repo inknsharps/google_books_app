9f22a9ad249e44db85912809f0cf282ebfec048f
inknsharps <nchen25@gmail.com>
2021-06-19 00:13:59 -0400

    add basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import Navbar from "./components/Navbar/NavbarContainer/NavbarContainer";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-    return <div className="App">
-        
-    </div>;
-}
+    return (
+    <Router>
+        <div className="App">
+            <Navbar/>
+            <Route exact path="/" component={ Search } />
+            <Route exact path="/saved" component={ Saved } />
+        </div>
+    </Router>
+    );
+};
 
 export default App;
